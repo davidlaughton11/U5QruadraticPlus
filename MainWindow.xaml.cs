707afbe0ccb204bc6c4020abfd61a8c189e33482
davidlaughton11 <47399940+davidlaughton11@users.noreply.github.com>
2019-05-23 21:52:44 -0400

    Add files via upload

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -17,6 +17,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+
 namespace u5QuadraticExtended
 {
     /// <summary>
@@ -26,10 +27,10 @@ namespace u5QuadraticExtended
     {
         public MainWindow()
         {
-            InitializeComponent();            
+            InitializeComponent();
         }
-
-        private void btnInitalize_Click(object sender, RoutedEventArgs e)
+        
+        private void btnInitalize_Click(object sender, RoutedEventArgs e )
         {
             double a;
             double b;
@@ -42,34 +43,109 @@ namespace u5QuadraticExtended
             double.TryParse(bInput.Text, out b);
             double.TryParse(cInput.Text, out c);
 
-            discriniment = b * b - 4 * a * c;
-
-            if (discriniment > 0)
+            if (string.IsNullOrWhiteSpace(aInput.Text))
             {
-                discriniment = System.Math.Sqrt(discriniment);
-                x = ((b * -1) + discriniment) / 2 * a;
-                x2 = ((b * -1) - discriniment) / 2 * a;
-                lblOutput.Content = "The two roots are x = " + x2 + " and x = " + x;
-            }
-            else if (discriniment < 0)
-            {
-                lblOutput.Content = "there are no roots";
-            }
-            else if (discriniment == 0)
-            {
-                x = ((b * -1) + discriniment) / 2 * a;
-                lblOutput.Content = "The sigle root is x = " + x;
+                MessageBox.Show("Enter values");
             }
 
-            Ellipse RootOne;
-            RootOne = new Ellipse();
-            RootOne.Height = 5;
-            RootOne.Width = 5;
-            RootOne.Fill = Brushes.Black;
-            RootOne.Margin = x + 150;
+            else if (string.IsNullOrWhiteSpace(bInput.Text))
+            {
+                MessageBox.Show("Enter values");
+            }
 
-            Graph.Children.Add(RootOne);
+            else if (string.IsNullOrWhiteSpace(cInput.Text))
+            {
+                MessageBox.Show("Enter values");
+            }
 
+            else
+            {                
+                discriniment = b * b - (4 * a * c);
+
+                if (discriniment > 0)
+                {
+                    discriniment = System.Math.Sqrt(discriniment);
+                    x = ((b * -1) + discriniment) / 2 * a;
+                    x2 = ((b * -1) - discriniment) / 2 * a;
+                    lblOutput.Content = "The two roots are x = " + x2 + " and x = " + x;
+
+                    Ellipse RootOne;
+                    RootOne = new Ellipse();
+                    RootOne.Height = 5;
+                    RootOne.Width = 5;
+                    RootOne.Fill = Brushes.Black;
+                    RootOne.Margin = new Thickness(x + 148, 98, 0, 0); ;
+                    Graph.Children.Add(RootOne);
+
+                    Ellipse RootTwo;
+                    RootTwo = new Ellipse();
+                    RootTwo.Height = 5;
+                    RootTwo.Width = 5;
+                    RootTwo.Fill = Brushes.Black;
+                    RootTwo.Margin = new Thickness(x2 + 148, 98, 0, 0); ;
+                    Graph.Children.Add(RootTwo);
+
+                    // Create pens.
+                    Pen redPen = new Pen(Brushes.Red, 3);
+                    Pen greenPen = new Pen(Brushes.Green, 3);
+
+                    // Create points that define curve.
+                    Point point1 = new Point(50, 50);
+                    Point point2 = new Point(100, 25);
+                    Point point3 = new Point(200, 5);
+                    Point point4 = new Point(250, 50);
+                    Point point5 = new Point(300, 100);
+                    Point point6 = new Point(350, 200);
+                    Point point7 = new Point(250, 250);
+                    Point[] curvePoints = { point1, point2, point3, point4, point5, point6, point7 };
+
+                    // Draw lines between original points to screen.
+                    e.Graphics.DrawLines(redPen, curvePoints);
+                    
+
+                    Ellipse Quadratic;
+                    Quadratic = new Ellipse();
+                    Quadratic.Height = 999;
+                    Quadratic.Width = 200;
+                    Quadratic.Stroke = Brushes.Black;
+
+                    double vertexY;
+                    double vertexX;
+                    vertexX = (x + x2) / 2;
+                    vertexY = a * (vertexX * vertexX) + b * vertexX + c; 
+
+                    if (a > 0)
+                    {
+                        RotateTransform rotateTransform1 = new RotateTransform(180, 0, 0);
+                        Quadratic.RenderTransform = rotateTransform1;
+                        Quadratic.Margin = new Thickness(x + 250 + vertexX, 100 + vertexY, 0, 0);
+                    }
+                    else if (a < 0)
+                    {
+                        Quadratic.Margin = new Thickness(x + 148 + vertexX, 100 + vertexY, 0, 0);
+                    }
+                    Graph.Children.Add(Quadratic);
+                }
+
+                else if (discriniment < 0)
+                {
+                    lblOutput.Content = "there are no roots";
+                }
+
+                else if (discriniment == 0)
+                {
+                    x = ((b * -1) + discriniment) / 2 * a;
+
+                    lblOutput.Content = "The sigle root is x = " + x;
+                    Ellipse RootOne;
+                    RootOne = new Ellipse();
+                    RootOne.Height = 5;
+                    RootOne.Width = 5;
+                    RootOne.Fill = Brushes.Black;
+                    RootOne.Margin = new Thickness(x + 148, 98, 0, 0);
+                    Graph.Children.Add(RootOne);                   
+                }
+            }                   
         }
     }
 }
